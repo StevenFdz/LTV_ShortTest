8eadd986887ac937ece165fbe588b4ab2466c384
StevenFdz <stevefdz14@gmail.com>
2022-04-27 00:27:05 -0600

    [new] Created the short_url algorithm
    +Created the short_url algorithm, the logical was written in the README, but in shorter, I take de URL, split it by "/", take for each of the words and select a random letter of the word, finally concat all the letter and add the quantify of registers in the DB.
    
    +Add the validation of URL, for did it, I use the URI library of Rubi, with it I have the possibility of open the URL and verify if this take me to a validate site
    
    +Update the click_count on indirect way, for do it when we access to a short_url, I take the id and the click_count of the request and send it to a function. In this function I update the register, with the id I get the correct register and with the count_click I can know how many I have to add(+1)
    
    +Order the most visited sites and show only 100, I use the functions of the ActiveRecord and later of did the sort, I limit the amount of registers showed

--- README.md
+++ README.md
@@ -23,6 +23,8 @@
 
     curl -X POST -d "full_url=https://google.com" http://localhost:3000/short_urls.json
 
+    curl -X POST -d "fullurl=https://www.ltvco.com/blog/ltv-work-from-home-six-months-later/" http://localhost:3000
+
 # Getting the top 100
 
     curl localhost:3000
@@ -30,3 +32,12 @@
 # Checking your short URL redirect
 
     curl -I localhost:3000/abc
+
+# Explanation of Algorithm for short URLs
+
+    1. Get the complete URL given for the user
+    2. Slice the URL by the "/", all the words or subdirectories of the url will be stored in an array
+    3. The before action give two('http'/'https', '') fields in the array that aren't usefull for us, so it remove them
+    4. Iterate the array created in the step 2, and get one random letter of the word in the index and concat with the final result, this for each index that the array has
+    5. Could happens that the URL has a lot of subdirectories so could be possible that the final result in this point has a lot of letters concataned, and this is not the idea, then in the case of the final result has more than 3 letters, the algorithm remove the last letter until the length will be 3
+    6. Finally, could happen that the final result will be repeated with another short url created before, so, for avoid this, at end of the final result the algorithm adds the count of registers that there are in the database in this moment
--- app/controllers/short_url_controller.rb
+++ app/controllers/short_url_controller.rb
@@ -6,27 +6,34 @@ class ShortUrlController < ApplicationController
 
   def index
     puts "INDEX"
-    short_urls = ShortUrl.order('click_count');
+    short_urls = ShortUrl.order('click_count desc').limit(100);
     render json: { status: "Success", message: "Short URLS Uploaded", data: short_urls}, status: :ok 
   end
 
   def create
     puts "CREATE"
     url = post_url[:fullurl]
-    puts url 
-    #Recuerda hacer la funcion de acortar la URL
-    short_url = ShortUrl.new({full_url: url, title: "GOO"})
-    if short_url.save
-      render json: { status: "Success", message: "Finished", data: short_url}, status: :ok
+    if validateURL(url)[0] === "200"
+      short_url = shorter_url(url)
+      fieldCollection = ShortUrl.new({full_url: url, title: short_url})
+      if fieldCollection.save
+        render json: { status: "Success", message: "Finished", data: fieldCollection}, status: :ok
+      else
+        render json: { status: "Failed", message: "Finished with Errors", data: fieldCollection.errors}, status: :unprocessable_entity
+      end
     else
-      render json: { status: "Failed", message: "Finished with Errors", data: short_url.errors}, status: :unprocessable_entity
+      render json: { status: "Failed", message: "Finished with Errors. URL invalidated"}, status: :unprocessable_entity
     end
   end
 
   def show
     puts "SHOW"
     fieldCollection = ShortUrl.where("title = ?", params[:title])
-    redirect_to fieldCollection.pluck(:full_url)[0]
+    if update_byId(fieldCollection.pluck(:id)[0], fieldCollection.pluck(:click_count)[0])
+      redirect_to fieldCollection.pluck(:full_url)[0]
+    else
+      render json: { status: "Failed", message: "Finished with Errors"}, status: :unprocessable_entity
+    end
 
   end
 
@@ -41,6 +48,15 @@ class ShortUrlController < ApplicationController
     end
   end
 
+  def update_byId(id, value)
+    fieldCollection = ShortUrl.find(id)
+    if fieldCollection.update_attributes({click_count: value+1})
+      return true
+    else
+      return false
+    end
+  end
+
 
   def post_url
     params.permit(:fullurl)
@@ -49,5 +65,42 @@ class ShortUrlController < ApplicationController
   def put_params
     params.permit(:id, :count)
   end
+
+  def shorter_url(url)
+    array = split_url(url)
+    short_url = ""
+    array.each { |element| short_url = short_url + element[rand(element.length)] }
+    fitShortUrl(short_url)
+    short_url = short_url + getRecords.to_s
+    return short_url
+  end
+
+  def split_url(url)
+    arr = url.split("/")
+    arr.shift()
+    arr.shift()
+    return arr
+  end
+
+  def fitShortUrl(short_url)
+    while short_url.length > 3
+      short_url.slice!(short_url.length-1)
+    end
+  end
+
+  def getRecords
+    return ShortUrl.count
+  end
+
+  def validateURL(url)
+    begin 
+      require 'open-uri'
+      return open(url).status
+    rescue
+      return ['404', "Not found"]
+    end
+    
+  end
+
 
 end
